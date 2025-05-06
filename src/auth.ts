0163f22d0ff1bf727af41afb5642e55fc113b79e
nereverine <74254439+nereverine@users.noreply.github.com>
2025-05-06 23:06:09 +0100

    refactor: improve error handling in signUp and login functions

--- src/auth.ts
+++ src/auth.ts
@@ -1,30 +1,40 @@
 import { supabase } from './supabaseClient';
 
 export const signUp = async (email: string, password: string) => {
-    const { data, error } = await supabase.auth.signUp({
-      email,
-      password,
-    });
-  
-    if (error) {
-      console.error(error.message); //change these to display toasts maybe
-      return null;
-    }
-    return data.user;
-  };
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
 
-export const login = async (email: string, password:string) => {
-    const {data, error} = await supabase.auth.signInWithPassword({
-        email,
-        password,
-    });
-    if(error){
-        console.error(error.message)
-        return null;
-    }
-    return data.user;
+  if (error) {
+    alert(error.message); //change these to display toasts maybe
+    return null;
+  }
+
+  //insert into profile table, the new user
+  const { error: profileError } = await supabase
+    .from('Profiles')
+    .insert([
+      {
+        id: data.user?.id
+      }]);
+
+  if (profileError) alert(profileError.message);
+  return data.user;
+};
+
+export const login = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) {
+    alert(error.message)
+    return null;
+  }
+  return data.user;
 }
 
 export const logout = async () => {
-    await supabase.auth.signOut();
+  await supabase.auth.signOut();
 }
