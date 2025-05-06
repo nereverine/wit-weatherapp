65feba4fc61b5ac372daa58036fb8cbc41734cdb
nereverine <74254439+nereverine@users.noreply.github.com>
2025-05-06 19:22:02 +0100

    switched to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </MantineProvider>
   </StrictMode>,
 )
